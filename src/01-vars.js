bcf8a327f1b3f9a0b5bffc0e7c698e4cf7838bfe
Ana Rotela <anarotela18@gmail.com>
2026-05-04 16:15:15 -0300

    Clase de var, let, const + Reto agregado

---
+++ src/01-vars.js
@@ -0,0 +1,14 @@
+var bloquesRojos = true;
+var bloquesRojos = 5;
+// console.log(bloquesRojos);
+
+let contador = 0;
+let mensaje  = 'hola';
+contador = 15;
+// console.log(contador)
+
+const PI = 3.14159;
+PI = 4;
+
+console.log(nombre); // undefined(nor error);
+var nombre = 'Oscar';
